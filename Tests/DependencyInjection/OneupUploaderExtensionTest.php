6a7158e0174adac36daae74e9fca822cc7b52d4c
Jim Schmid <jim.schmid@1up.io>
2013-03-08 18:53:49 +0100

    Added test setup including a dummy test for travis tests.

---
+++ Tests/DependencyInjection/OneupUploaderExtensionTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Oneup\UploaderBundle\Tests\DependencyInjection;
+
+class OneupUploaderExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testDummyForSetup()
+    {
+        $this->assertTrue(true);
+    }
+}
