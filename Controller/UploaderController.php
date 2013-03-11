e3026276c3009cd92bb77726beae4144444ca53d
Jim Schmid <jim.schmid@1up.io>
2013-03-11 19:09:38 +0100

    Added service_container to the list of arguments of the default UploaderController.

--- Controller/UploaderController.php
+++ Controller/UploaderController.php
@@ -5,14 +5,20 @@ namespace Oneup\UploaderBundle\Controller;
 class UploaderController
 {
     protected $mappings;
+    protected $container;
     
-    public function __construct($mappings)
+    public function __construct($mappings, $container)
     {
-        $this->mappings = $mappings;
+        $this->mappings  = $mappings;
+        $this->container = $container;
     }
     
     public function upload($mapping)
     {
+        $container = $this->container;
+        $config = $this->mappings[$mapping];
         
+        if(!$container->has($config['storage']))
+            throw new \InvalidArgumentException(sprintf('The storage service "%s" must be defined.'));
     }
 }
