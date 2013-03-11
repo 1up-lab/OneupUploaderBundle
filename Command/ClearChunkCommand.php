59fc81f467dde376f02d4f38913ce834dacdf878
Jim Schmid <jim.schmid@1up.io>
2013-03-11 13:40:26 +0100

    Added a Command for clearing chunks: oneup:uploader:clear-chunks

---
+++ Command/ClearChunkCommand.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Oneup\UploaderBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ClearChunkCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('oneup:uploader:clear-chunks')
+            ->setDescription('Clear chunks according to the max-age you defined in your configuration.')
+        ;
+    }
+    
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $manager = $this->getContainer()->get('oneup_uploader.chunks.manager');
+        $manager->clear();
+    }
+}
