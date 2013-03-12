8bf36d43c434f4e1ca715f34b4fe116623946db6
Jim Schmid <jim.schmid@1up.io>
2013-03-12 11:20:26 +0100

    Added a command to clear orphaned uploads.

---
+++ Command/ClearOrphansCommand.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Oneup\UploaderBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ClearOrphansCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('oneup:uploader:clear-orphans')
+            ->setDescription('Clear orphaned uploads according to the max-age you defined in your configuration.')
+        ;
+    }
+    
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $manager = $this->getContainer()->get('oneup_uploader.orphanage');
+        $manager->clear();
+    }
+}
