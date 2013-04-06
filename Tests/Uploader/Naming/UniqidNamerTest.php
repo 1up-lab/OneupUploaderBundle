c7c92b186d091523734db5585c15f73bbb820603
Jim Schmid <jim.schmid@1up.io>
2013-04-06 10:30:00 +0200

    Added Tests for UniqidNamer

---
+++ Tests/Uploader/Naming/UniqidNamerTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Oneup\UploaderBundle\Tests\Uploader\Naming;
+
+use Oneup\UploaderBundle\Uploader\Naming\UniqidNamer;
+
+class UniqidNamerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testNamerReturnsName()
+    {
+        $file = $this->getMockBuilder('Symfony\Component\HttpFoundation\File\UploadedFile')
+            ->disableOriginalConstructor()
+            ->getMock()
+        ;
+        
+        $file
+            ->expects($this->any())
+            ->method('guessExtension')
+            ->will($this->returnValue('jpeg'))
+        ;
+        
+        $namer = new UniqidNamer();
+        $this->assertRegExp('/[a-z0-9]{13}.jpeg/', $namer->name($file));
+    }
+    
+    public function testNamerReturnsUniqueName()
+    {
+        $file = $this->getMockBuilder('Symfony\Component\HttpFoundation\File\UploadedFile')
+            ->disableOriginalConstructor()
+            ->getMock()
+        ;
+        
+        $file
+            ->expects($this->any())
+            ->method('guessExtension')
+            ->will($this->returnValue('jpeg'))
+        ;
+        
+        $namer = new UniqidNamer();
+        
+        // get two different names
+        $name1 = $namer->name($file);
+        $name2 = $namer->name($file);
+        
+        $this->assertNotEquals($name1, $name2);
+    }
+}
