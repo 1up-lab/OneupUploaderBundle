66e395c1eebcc93b3cf2ac70d2cba22c096e4e3f
Jim Schmid <jim.schmid@1up.io>
2013-04-06 15:56:13 +0200

    Mocking madness.. Tested Controller, the first part.

---
+++ Tests/Controller/ControllerTest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Oneup\UploaderBundle\Tests\Controller;
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+use Oneup\UploaderBundle\Uploader\Naming\UniqidNamer;
+use Oneup\UploaderBundle\Uploader\Storage\FilesystemStorage;
+use Oneup\UploaderBundle\Controller\UploaderController;
+
+class ControllerTest extends \PHPUnit_Framework_TestCase
+{
+    protected $tempFile;
+    
+    public function setUp()
+    {
+        // create temporary file
+        $this->tempFile = tempnam(sys_get_temp_dir(), 'uploader');
+        
+        $pointer = fopen($this->tempFile, 'w+');
+        fwrite($pointer, str_repeat('A', 1024), 1024);
+        fclose($pointer);
+    }
+    
+    public function testUpload()
+    {
+        $container = $this->getContainerMock();
+        $storage = new FilesystemStorage(sys_get_temp_dir() . '/uploader');
+        $config = array(
+            'use_orphanage' => false,
+            'namer' => 'namer',
+            'max_size' => 2048,
+            'allowed_extensions' => array(),
+            'disallowed_extensions' => array()
+        );
+        
+        $controller = new UploaderController($container, $storage, $config, 'cat');
+        $controller->upload();
+        
+        // check if original file has been moved
+        $this->assertFalse(file_exists($this->tempFile));
+    }
+    
+    protected function getContainerMock()
+    {
+        $mock = $this->getMock('Symfony\Component\DependencyInjection\ContainerInterface');
+        $mock
+            ->expects($this->any())
+            ->method('get')
+            ->will($this->returnCallback(array($this, 'containerGetCb')))
+        ;
+        
+        return $mock;
+    }
+    
+    public function containerGetCb($inp)
+    {
+        if($inp == 'request')
+            return $this->getRequestMock();
+        
+        if($inp == 'event_dispatcher')
+            return $this->getEventDispatcherMock();
+        
+        if($inp == 'namer')
+            return new UniqidNamer();
+    }
+    
+    protected function getEventDispatcherMock()
+    {
+        $mock = $this->getMock('Symfony\Component\EventDispatcher\EventDispatcherInterface');
+        $mock
+            ->expects($this->any())
+            ->method('dispatch')
+            ->will($this->returnValue(true))
+        ;
+        
+        return $mock;
+    }
+    
+    protected function getRequestMock()
+    {
+        $mock = $this->getMock('Symfony\Component\HttpFoundation\Request');
+        $mock
+            ->expects($this->any())
+            ->method('get')
+            ->with('qqtotalparts')
+            ->will($this->returnValue(1))
+        ;
+        
+        $mock->files = array(
+            $this->getUploadedFile()
+        );
+        
+        return $mock;
+    }
+    
+    protected function getUploadedFile()
+    {
+        return new UploadedFile($this->tempFile, 'grumpy-cat.jpeg', 'image/jpeg', 1024, null, true);
+    }
+}
