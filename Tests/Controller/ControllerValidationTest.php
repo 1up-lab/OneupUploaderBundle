89e84169a46cf48796fc2734866554f8d3554115
Jim Schmid <jim.schmid@1up.io>
2013-04-06 12:51:40 +0200

    Testing one third of the validations in the controller.

---
+++ Tests/Controller/ControllerValidationTest.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Oneup\UploaderBundle\Tests\Controller;
+
+use Oneup\UploaderBundle\Controller\UploaderController;
+
+class ControllerValidationTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @expectedException Symfony\Component\HttpFoundation\File\Exception\UploadException
+     */
+    public function testMaxSizeValidationFails()
+    {
+        // create a config
+        $config = array();
+        $config['max_size'] = 10;
+        $config['allowed_types'] = array();
+        $config['disallowed_types'] = array();
+        
+        // prepare mock
+        $file = $this->getUploadedFileMock();
+        $method = $this->getValidationMethod();
+        
+        $container = $this->getContainerMock();
+        $storage = $this->getStorageMock();
+        
+        $controller = new UploaderController($container, $storage, $config, 'cat');
+        $method->invoke($controller, $file);
+    }
+    
+    public function testMaxSizeValidationPasses()
+    {
+        // create a config
+        $config = array();
+        $config['max_size'] = 20;
+        $config['allowed_types'] = array();
+        $config['disallowed_types'] = array();
+        
+        // prepare mock
+        $file = $this->getUploadedFileMock();
+        $method = $this->getValidationMethod();
+        
+        $container = $this->getContainerMock();
+        $storage = $this->getStorageMock();
+        
+        $controller = new UploaderController($container, $storage, $config, 'cat');
+        $method->invoke($controller, $file);
+        
+        // yey, no exception thrown
+        $this->assertTrue(true);
+    }
+    
+    protected function getUploadedFileMock()
+    {
+        $file = $this->getMockBuilder('Symfony\Component\HttpFoundation\File\UploadedFile')
+            ->disableOriginalConstructor()
+            ->getMock()
+        ;
+        
+        $file
+            ->expects($this->any())
+            ->method('getClientSize')
+            ->will($this->returnValue(15))
+        ;
+        
+        $file
+            ->expects($this->any())
+            ->method('getClientOriginalName')
+            ->will($this->returnValue('grumpycat.jpeg'))
+        ;
+        
+        return $file;
+    }
+    
+    protected function getContainerMock()
+    {
+        return $this->getMock('Symfony\Component\DependencyInjection\ContainerInterface');
+    }
+    
+    protected function getStorageMock()
+    {
+        return $this->getMock('Oneup\UploaderBundle\Uploader\Storage\StorageInterface');
+    }
+    
+    protected function getValidationMethod()
+    {
+        // create a public version of the validate method
+        $class = new \ReflectionClass('Oneup\\UploaderBundle\\Controller\\UploaderController');
+        $method = $class->getMethod('validate');
+        $method->setAccessible(true);
+        
+        return $method;
+    }
+}
