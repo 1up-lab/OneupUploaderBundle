5da645b523edb24f79a0b9360b43bfa196c3276d
Jim Schmid <jim.schmid@1up.io>
2013-04-12 15:40:28 +0200

    First version of Plupload implementation.

---
+++ Controller/PluploadController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Oneup\UploaderBundle\Controller;
+
+use Symfony\Component\HttpFoundation\File\Exception\UploadException;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+use Oneup\UploaderBundle\Controller\AbstractChunkedController;
+use Oneup\UploaderBundle\Uploader\Response\EmptyResponse;
+
+class PluploadController extends AbstractChunkedController
+{
+    public function upload()
+    {
+        $request = $this->container->get('request');
+        $response = new EmptyResponse();
+        $files = $request->files;
+        
+        foreach($files as $file)
+        {
+            try
+            {
+                $uploaded = $this->handleUpload($file);
+                
+                // dispatch POST_PERSIST AND POST_UPLOAD events
+                $this->dispatchEvents($uploaded, $response, $request);
+            }
+            catch(UploadException $e)
+            {
+                // return nothing
+                return new JsonResponse(array());
+            }
+        }
+        
+        return new JsonResponse($response->assemble());
+    }
+    
+    protected function parseChunkedRequest(Request $request)
+    {
+        die("foobar");
+    }
+}
