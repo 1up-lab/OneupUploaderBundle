ced5635c00f4bb9db4f76804b9e5d3345f54c948
Jim Schmid <jim.schmid@1up.io>
2013-04-22 19:22:43 +0200

    Added validator to validate allowed extensions.

---
+++ EventListener/AllowedExtensionValidationListener.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Oneup\UploaderBundle\EventListener;
+
+use Oneup\UploaderBundle\Event\ValidationEvent;
+use Oneup\UploaderBundle\Uploader\Exception\ValidationException;
+
+class AllowedExtensionValidationListener
+{
+    public function onValidate(ValidationEvent $event)
+    {
+        $config = $event->getConfig();
+        $file   = $event->getFile();
+        
+        $extension = pathinfo($file->getClientOriginalName(), PATHINFO_EXTENSION);
+        
+        if(count($config['allowed_extensions']) > 0 && !in_array($extension, $config['allowed_extensions'])) {
+            throw new ValidationException('error.whitelist');
+        }
+    }
+}
