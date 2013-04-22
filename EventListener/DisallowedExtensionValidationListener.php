5baa9266d9e063bc2ee2b92d3d7ab5761559f188
Jim Schmid <jim.schmid@1up.io>
2013-04-22 19:36:05 +0200

    Added validator to validate disallowed extensions.

---
+++ EventListener/DisallowedExtensionValidationListener.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Oneup\UploaderBundle\EventListener;
+
+use Oneup\UploaderBundle\Event\ValidationEvent;
+use Oneup\UploaderBundle\Uploader\Exception\ValidationException;
+
+class DisallowedExtensionValidationListener
+{
+    public function onValidate(ValidationEvent $event)
+    {
+        $config = $event->getConfig();
+        $file   = $event->getFile();
+        
+        $extension = pathinfo($file->getClientOriginalName(), PATHINFO_EXTENSION);
+        
+        if(count($config['disallowed_extensions']) > 0 && in_array($extension, $config['disallowed_extensions'])) {
+            throw new ValidationException('error.blacklist');
+        }
+    }
+}
