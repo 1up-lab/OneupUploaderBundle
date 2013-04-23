7cb178103ce4e61f609426eb6bc961d593864aea
Jim Schmid <jim.schmid@1up.io>
2013-04-23 21:12:16 +0200

    Added the possibility to validate an uploaded file against its mimetype.

---
+++ EventListener/AllowedMimetypeValidationListener.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Oneup\UploaderBundle\EventListener;
+
+use Oneup\UploaderBundle\Event\ValidationEvent;
+use Oneup\UploaderBundle\Uploader\Exception\ValidationException;
+
+class AllowedMimetypeValidationListener
+{
+    public function onValidate(ValidationEvent $event)
+    {
+        $config = $event->getConfig();
+        $file   = $event->getFile();
+        
+        if(count($config['allowed_mimetypes']) == 0) {
+            return;
+        }
+        
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $mimetype = finfo_file($finfo, $file->getRealpath());
+        
+        if(!in_array($mimetype, $config['allowed_mimetypes'])) {
+            throw new ValidationException('error.whitelist');
+        }
+    }
+}
