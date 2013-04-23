86efe3662c06ca6decbf997d9528bcae3e8c9fd0
Jim Schmid <jim.schmid@1up.io>
2013-04-23 21:16:02 +0200

    Blacklist test against a files mime type.

---
+++ EventListener/DisallowedMimetypeValidationListener.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Oneup\UploaderBundle\EventListener;
+
+use Oneup\UploaderBundle\Event\ValidationEvent;
+use Oneup\UploaderBundle\Uploader\Exception\ValidationException;
+
+class DisallowedMimetypeValidationListener
+{
+    public function onValidate(ValidationEvent $event)
+    {
+        $config = $event->getConfig();
+        $file   = $event->getFile();
+        
+        if(count($config['disallowed_mimetypes']) == 0) {
+            return;
+        }
+        
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $mimetype = finfo_file($finfo, $file->getRealpath());
+        
+        if(in_array($mimetype, $config['disallowed_mimetypes'])) {
+            throw new ValidationException('error.blacklist');
+        }
+    }
+}
