579bad5541b97463bfc31275a0779de9a6f850f9
Jim Schmid <jim.schmid@1up.io>
2013-04-28 10:50:48 +0200

    CS fixes.

--- Uploader/Response/AbstractResponse.php
+++ Uploader/Response/AbstractResponse.php
@@ -19,10 +19,12 @@ abstract class AbstractResponse implements ResponseInterface
     {
         is_null($offset) ? $this->data[] = $value : $this->data[$offset] = $value;
     }
+    
     public function offsetExists($offset)
     {
         return isset($this->data[$offset]);
     }
+    
     public function offsetUnset($offset)
     {
         unset($this->data[$offset]);
