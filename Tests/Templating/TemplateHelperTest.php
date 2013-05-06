579231a07b75193336c18d082fab862af8c8a00e
Jim Schmid <jim.schmid@1up.io>
2013-05-06 22:43:48 +0200

    Added TemplateHelperTest for code coverage completion.

---
+++ Tests/Templating/TemplateHelperTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Oneup\UploaderBundle\Tests\Uploader\Chunk;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class TemplateHelperTest extends WebTestCase
+{
+    public function testName()
+    {
+        $client = static::createClient();
+        $container = $client->getContainer();
+        
+        $helper = $container->get('oneup_uploader.templating.uploader_helper');
+        
+        // this is for code coverage
+        $this->assertEquals($helper->getName(), 'oneup_uploader');
+    }
+}
