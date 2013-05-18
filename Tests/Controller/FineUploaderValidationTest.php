161a0e266ad222c1adc0d399c553a138386bb23e
Jim Schmid <jim.schmid@1up.io>
2013-05-18 16:58:29 +0200

    Added Validation test for FineUploader.

---
+++ Tests/Controller/FineUploaderValidationTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Oneup\UploaderBundle\Tests\Controller;
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Oneup\UploaderBundle\Tests\Controller\AbstractValidationTest;
+
+class FineUploaderValidationTest extends AbstractValidationTest
+{
+    protected function getConfigKey()
+    {
+        return 'fineuploader_validation';
+    }
+    
+    protected function getRequestParameters()
+    {
+        return array();
+    }
+    
+    protected function getFileWithCorrectExtension()
+    {
+        return new UploadedFile(
+            $this->createTempFile(128),
+            'cat.ok',
+            'text/plain',
+            128
+        );
+    }
+    
+    protected function getFileWithIncorrectExtension()
+    {
+        return new UploadedFile(
+            $this->createTempFile(128),
+            'cat.fail',
+            'text/plain',
+            128
+        );
+    }
+    
+    protected function getFileWithCorrectMimeType()
+    {
+        return new UploadedFile(
+            $this->createTempFile(128),
+            'cat.ok',
+            'image/jpg',
+            128
+        );
+    }
+    
+    protected function getFileWithIncorrectMimeType()
+    {
+        return new UploadedFile(
+            $this->createTempFile(128),
+            'cat.ok',
+            'image/gif',
+            128
+        );
+    }
+}
