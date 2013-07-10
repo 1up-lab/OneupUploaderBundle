a643e90e12f2a603a3b8682095c4f2af9eb0643a
Tamás Millián <tamas@labficiency.com>
2013-07-10 12:38:28 +0200

    Updated blueimp frontend to return errors from validation in the response, extended the abstractresponse

--- Uploader/Response/AbstractResponse.php
+++ Uploader/Response/AbstractResponse.php
@@ -13,6 +13,32 @@ abstract class AbstractResponse implements \ArrayAccess, ResponseInterface
         $this->data = array();
     }
 
+    /**
+     * The \ArrayAccess interface does not support multi-dimensional array syntax such as $array["foo"][] = bar
+     * This function will take a path of arrays and add a new element to it.
+     *
+     * @param mixed $value
+     * @param string $offset,...
+     *
+     * @throws \InvalidArgumentException if the path contains non-array or unset items.
+     *
+     */
+    public function addToOffset()
+    {
+        $args = func_get_args();
+        $value = $args[0];
+        array_shift($args);
+        $element =& $this->data;
+        foreach ($args as $offset) {
+            if (isset($element[$offset]) && is_array($element[$offset])) {
+                $element =& $element[$offset];
+            } else {
+                throw new \InvalidArgumentException("The specified path does not exsist or is not an array at " . $offset);
+            }
+        }
+        $element = $value;
+    }
+
     public function offsetSet($offset, $value)
     {
         is_null($offset) ? $this->data[] = $value : $this->data[$offset] = $value;
