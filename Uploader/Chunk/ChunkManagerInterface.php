eb40fefab5a195b30bb9af3b6ca1923e2068c4bf
Jim Schmid <jim.schmid@1up.io>
2013-07-25 17:50:20 +0200

    Added new parameters to ChunkManagerInterface.

--- Uploader/Chunk/ChunkManagerInterface.php
+++ Uploader/Chunk/ChunkManagerInterface.php
@@ -8,7 +8,7 @@ interface ChunkManagerInterface
 {
     public function clear();
     public function addChunk($uuid, $index, UploadedFile $chunk, $original);
-    public function assembleChunks(\IteratorAggregate $chunks);
+    public function assembleChunks(\IteratorAggregate $chunks, $removeChunk = true, $renameChunk = false);
     public function cleanup($path);
     public function getChunks($uuid);
 }
