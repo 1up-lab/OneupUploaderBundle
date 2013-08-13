c0d9cce3e2c5b2ecb6253d719d8471049451ae2b
Jim Schmid <jim.schmid@1up.io>
2013-08-13 09:36:44 +0200

    Added documentation headers to ChunkManagerInterface.

--- Uploader/Chunk/ChunkManagerInterface.php
+++ Uploader/Chunk/ChunkManagerInterface.php
@@ -6,9 +6,50 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 interface ChunkManagerInterface
 {
-    public function clear();
+    /**
+     * Adds a new Chunk to a given uuid.
+     *
+     * @param string       $uuid
+     * @param int          $index
+     * @param UploadedFile $chunk
+     * @param string       $original The file name of the original file
+     *
+     * @return File The moved chunk file.
+     */
     public function addChunk($uuid, $index, UploadedFile $chunk, $original);
+
+    /**
+     * Assembles the given chunks and return the resulting file.
+     *
+     * @param \IteratorAggregate $chunks
+     * @param bool               $removeChunk Remove the chunk file once its assembled.
+     * @param bool               $renameChunk Rename the chunk file once its assembled.
+     *
+     * @return File
+     */
     public function assembleChunks(\IteratorAggregate $chunks, $removeChunk = true, $renameChunk = false);
-    public function cleanup($path);
+
+    /**
+     * Get chunks associated with the given uuid.
+     *
+     * @param string $uuid
+     *
+     * @return Finder A finder instance
+     */
     public function getChunks($uuid);
+
+    /**
+     * Clean a given path.
+     *
+     * @param  string $path
+     * @return bool
+     */
+    public function cleanup($path);
+
+    /**
+     * Clears the chunk manager directory. Remove all files older than the configured maxage.
+     *
+     * @return void
+     */
+    public function clear();
 }
