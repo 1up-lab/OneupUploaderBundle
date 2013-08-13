01718556b0a231a26930fd5e32550992b3d1e350
Jim Schmid <jim.schmid@1up.io>
2013-08-13 16:23:31 +0200

    Removed unused dependencies in tests.

--- Tests/App/AppKernel.php
+++ Tests/App/AppKernel.php
@@ -10,9 +10,7 @@ class AppKernel extends Kernel
         $bundles = array(
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-            new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new JMS\SerializerBundle\JMSSerializerBundle($this),
 
             // bundle to test
             new Oneup\UploaderBundle\OneupUploaderBundle(),
