47c9e32a13e4cec964845e6ad09708643b66fe79
mitom <tamas.millian@gmail.com>
2013-10-14 11:18:00 +0200

    remove file from gaufrette chunk storage after upload

--- Uploader/Storage/GaufretteStorage.php
+++ Uploader/Storage/GaufretteStorage.php
@@ -5,6 +5,7 @@ namespace Oneup\UploaderBundle\Uploader\Storage;
 use Oneup\UploaderBundle\Uploader\File\FileInterface;
 use Oneup\UploaderBundle\Uploader\File\GaufretteFile;
 use Gaufrette\Filesystem;
+use Symfony\Component\Filesystem\Filesystem as LocalFilesystem;
 use Gaufrette\Adapter\MetadataSupporter;
 use Oneup\UploaderBundle\Uploader\Gaufrette\StreamManager;
 
@@ -39,8 +40,14 @@ class GaufretteStorage extends StreamManager implements StorageInterface
         $dst = $this->filesystem->createStream($path);
 
         $this->openStream($dst, 'w');
-
         $this->stream($file, $dst);
+
+        if ($file instanceof GaufretteFile) {
+            $file->delete();
+        } else {
+            $filesystem = new LocalFilesystem();
+            $filesystem->remove($file->getPathname());
+        }
 
         return new GaufretteFile($this->filesystem->get($path), $this->filesystem, $this->streamWrapperPrefix);
     }
