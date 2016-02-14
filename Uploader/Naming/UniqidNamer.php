bd824ccc8bed9eca4e094f546c51e817258dedfd
nomadOnWeb <nomadonweb@gmail.com>
2016-02-14 06:01:53 +0100

    Update UniqidNamer.php

--- Uploader/Naming/UniqidNamer.php
+++ Uploader/Naming/UniqidNamer.php
@@ -2,11 +2,13 @@
 
 namespace Oneup\UploaderBundle\Uploader\Naming;
 
+use Symfony\Component\HttpFoundation\Request;
+
 use Oneup\UploaderBundle\Uploader\File\FileInterface;
 
 class UniqidNamer implements NamerInterface
 {
-    public function name(FileInterface $file, $request)
+    public function name(FileInterface $file, Request $request)
     {
         $subfolder = '';
         if ($subfolder = $request->get('subfolder')) $subfolder .= '/';
