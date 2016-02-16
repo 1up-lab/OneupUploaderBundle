0934c52e33609e470a08dab3c044696466c5eeeb
Ertho <robin.calmejane@gmail.com>
2016-02-16 15:18:34 +0100

    Update UniqidNamer.php

--- Uploader/Naming/UniqidNamer.php
+++ Uploader/Naming/UniqidNamer.php
@@ -8,6 +8,14 @@ use Oneup\UploaderBundle\Uploader\File\FileInterface;
 
 class UniqidNamer implements NamerInterface
 {
+    /**
+     * Name a given file and return the name
+     *
+     * @param  FileInterface $file
+     * @param Request $request The request object.
+     * 
+     * @return string
+     */
     public function name(FileInterface $file, Request $request)
     {
         $subfolder = '';
