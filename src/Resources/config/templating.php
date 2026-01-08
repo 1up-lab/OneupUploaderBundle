2025c655bcf5e754d2e6083fba148e9ff6802423
borisf <borisf@miltenyi.com>
2026-01-08 11:57:15 +0100

    fixed code-style

--- src/Resources/config/templating.php
+++ src/Resources/config/templating.php
@@ -6,7 +6,7 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 use Oneup\UploaderBundle\Templating\Helper\UploaderHelper;
 
-return static function (ContainerConfigurator $container) {
+return static function (ContainerConfigurator $container): void {
     $services = $container->services();
 
     $services->set('oneup_uploader.templating.uploader_helper', UploaderHelper::class)
